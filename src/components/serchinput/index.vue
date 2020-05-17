242341c07cdafd0b2168bc6bc2a5fbb711115eb9
冯宇 <5582209+feng_yu1999@user.noreply.gitee.com>
2020-05-17 23:00:31 +0800

    搜索组件、轮播、导航

--- src/components/serchinput/index.vue
+++ src/components/serchinput/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <view>输入框</view>
+  <!-- 搜索组件 -->
+  <view class="search_input">
+    <!-- 跳转组件 -->
+    <navigator url="/pages/search/index">搜索</navigator>
+  </view>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+.search_input {
+  height: 90rpx;
+  padding: 10rpx;
+  background-color: #eb4450;
+  navigator {
+    text-align: center;
+    color: #ccc;
+    height: 100%;
+    line-height: 90rpx;
+    background-color: azure;
+    border-radius: 15rpx; //边框圆角
+  }
+}
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,23 @@
 <template>
   <view>
-    首页
+	  <!-- 搜索 -->
     <serchInput></serchInput>
+	<!-- 轮播 -->
+	<swiper class="swiperbox" indicator-dots autoplay interval="3000" duration="1000" circular>
+		<navigator :url="item.navigator_url">
+		<swiper-item v-for="item in swiperlist" :key="item.goods_id">
+			<image :src="item.image_src" mode="widthFix"></image>
+		</swiper-item>
+		</navigator>
+	</swiper>
+	<!-- 首页导航 -->
+	<view class="navbox">
+		<view class="nav_item" v-for="item in navlist" :key="item.name">
+			<navigator :url="item.navigator_url">
+			<image :src="item.image_src" mode="widthFix"></image>
+			</navigator>
+		</view>
+	</view>
   </view>
 </template>
 
@@ -9,15 +25,56 @@
 import serchInput from "@/components/serchinput/index";
 export default {
   data() {
-    return {};
+    return {
+	  swiperlist: [], //轮播图数据
+	  navlist:[]      //首页导航数据
+    };
   },
   onLoad() {},
-  methods: {},
+  mounted() {
+	this.getswiperlist(); //获取轮播图方法
+	this.getnavlist()     //获取首页导航
+  },
+  methods: {
+    async getswiperlist() {
+      let res = await uni.request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"
+	  });
+	  this.swiperlist=res[1].data.message
+      console.log(res[1].data.message);
+	},
+	async getnavlist(){
+	let res=await	uni.request({
+			url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"
+		})
+		this.navlist=res[1].data.message
+		console.log(res[1].data.message)
+	}
+  },
   components: {
     serchInput
   }
 };
 </script>
 
-<style>
+<style lang="scss">
+.swiperbox{
+	width: 750rpx;
+	height: 340rpx;
+	padding: 5rpx;
+		image{
+			width: 100%;
+		}
+}
+// 导航样式
+.navbox{
+	display: flex;
+	padding: 10rpx;
+	.nav_item{
+		flex: 1;
+		image{
+			width: 100%;
+		}
+	}
+}
 </style>
