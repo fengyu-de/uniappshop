fde1f3251fcbe69d128007050dd11dba2d72f22f
冯宇 <5582209+feng_yu1999@user.noreply.gitee.com>
2020-05-18 14:53:42 +0800

    首页完成

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <view>分类</view>
+  <view></view>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      categorieslist: [] //商品分类数据
+    };
+  },
+  mounted() {
+    this.getlist(); //获取分类数据
+  },
+  methods: {
+    async getlist() {
+      let res = await uni.request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+      });
+      this.categorieslist = res[1].data.message;
+      console.log(res[1].data.message);
+    }
+  }
+};
 </script>
 
 <style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -18,6 +18,19 @@
 			</navigator>
 		</view>
 	</view>
+	<!-- 楼层 -->
+	<view class="floordatabox">
+		<view class="floordata_item" v-for="(item,index) in  floordatalist" :key="index">
+			<view class="title">
+				<image :src="item.floor_title.image_src" mode="widthFix"></image>
+			</view>
+			<view class="list" v-for="item2 in item.product_list" :key="item2.name">
+				<navigator>
+			<image :src="item2.image_src" mode="widthFix"></image>
+			</navigator>
+			</view>
+		</view>
+	</view>
   </view>
 </template>
 
@@ -27,13 +40,15 @@ export default {
   data() {
     return {
 	  swiperlist: [], //轮播图数据
-	  navlist:[]      //首页导航数据
+	  navlist:[] ,     //首页导航数据
+	  floordatalist:[]  //楼层数据
     };
   },
   onLoad() {},
   mounted() {
 	this.getswiperlist(); //获取轮播图方法
 	this.getnavlist()     //获取首页导航
+	this.getfloordata()    //获取楼层数据
   },
   methods: {
     async getswiperlist() {
@@ -41,13 +56,20 @@ export default {
         url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"
 	  });
 	  this.swiperlist=res[1].data.message
-      console.log(res[1].data.message);
+    //   console.log(res[1].data.message);
 	},
 	async getnavlist(){
 	let res=await	uni.request({
 			url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"
 		})
 		this.navlist=res[1].data.message
+		// console.log(res[1].data.message)
+	},
+	async getfloordata(){
+	let res=await	uni.request({
+			url:"https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"
+		})
+		this.floordatalist=res[1].data.message
 		console.log(res[1].data.message)
 	}
   },
@@ -77,4 +99,27 @@ export default {
 		}
 	}
 }
+.floordatabox{
+	.floordata_item{
+		.title{
+			image{
+				width: 100%;
+				display:inline-block
+			}
+		}
+		.list{
+			float: left;
+			width: 33.3%;
+			navigator{
+			}
+			image{
+		width: 100%;
+			}
+		}
+		.list:nth-last-child(-n+4){
+			height: 230rpx;
+		}
+	}
+}
+
 </style>
